70833628c6b95ac1770a540cd844cf11a706cbe6
DaMrStick <76888971+DaMrStick@users.noreply.github.com>
2024-03-24 22:08:59 +1100

    done

--- src/main/java/me/iron_fist222/custombosses/Bosses/Moves/MoveTemplate.java
+++ src/main/java/me/iron_fist222/custombosses/Bosses/Moves/MoveTemplate.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import org.checkerframework.checker.units.qual.C;
 
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
@@ -39,6 +40,10 @@ public class MoveTemplate extends FallingBlockEntity {
         this.setDeltaMovement(velocity);
         this.setNoGravity(true);
         this.blocks = SummonMove(world,defLoc,block,velocity,facingDir,size,cls);
+        CompoundTag blockState = new CompoundTag();
+        this.addAdditionalSaveData(blockState);
+        blockState.put("BlockState",NbtUtils.writeBlockState(block));
+        this.readAdditionalSaveData(blockState);
     }
 
     public static List SummonMove(Level world, Vec3 centralPos, BlockState block, Vec3 velocity, Vec3 facingDir, int size, Class<MoveTemplate> cls){
@@ -46,15 +51,32 @@ public class MoveTemplate extends FallingBlockEntity {
         Vec3 curentPos = centralPos;
         Vec3 rVector = facingDir.cross(new Vec3(0,1,0));
         Vec3 uVector = facingDir.cross(rVector);
-        System.out.println(rVector);
-        System.out.println(uVector);
-
+        Vec3 tempVector;
+        Vec3 multiplier = new Vec3(1,1,1);
         if(size > 1) {
-            for (int index = 0; index < (size ^ 2); index++) {
+            int loopSize = (int) (2*Math.pow(size,2)-size*2);
+            for (int index = 0; index < loopSize; index++) {
                 try {
+                    tempVector = new Vec3(0,0,0);
+                    curentPos = centralPos;
                     Constructor<MoveTemplate> ctr = cls.getConstructor(Level.class, Vec3.class, BlockState.class);
                     MoveTemplate currentBlock = ctr.newInstance(world, centralPos, block);
-                    curentPos = curentPos.add(rVector);
+                    float rSqrt = Math.round(Math.sqrt(index));
+                    int remainder = index%2;
+                    float NextLevelRemainder = (float) (Math.round((2+Math.sqrt(4+8*index))/(4)) - (2+Math.sqrt(4+8*index))/(4));
+                    if (NextLevelRemainder == 0){
+                        multiplier = multiplier.add(new Vec3(1,1,1));
+                    }
+                    if(remainder == 0){
+                        tempVector = rVector;
+                        multiplier = multiplier.multiply(new Vec3(-1,-1,-1));
+                    }else{
+                        tempVector = uVector;
+                    }
+
+                    tempVector = tempVector.multiply(multiplier);
+                    curentPos = curentPos.add(tempVector);
+                    System.out.println(multiplier);
                     currentBlock.teleportTo(curentPos.x,curentPos.y,curentPos.z);
                     currentBlock.setDeltaMovement(velocity);
                     currentBlock.setNoGravity(true);
@@ -68,6 +90,14 @@ public class MoveTemplate extends FallingBlockEntity {
         return moveBlocks;
     }
 
+    private static MoveTemplate createBlock(Level world,int current){
+        if (current == 0){
+            return createBlock(world,current-1);
+        }
+
+        return new MoveTemplate(world);
+
+    }
 
     @Override
     public void tick() {
