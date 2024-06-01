70807081f051b1a16e9eed9e1fcfb0e15913323a
DaMrStick <76888971+DaMrStick@users.noreply.github.com>
2024-06-01 11:18:11 +1000

    selectiojn clicks

--- src/main/java/me/iron_fist222/custombosses/Bosses/Moves/ParticleMoveTemplate.java
+++ src/main/java/me/iron_fist222/custombosses/Bosses/Moves/ParticleMoveTemplate.java
@@ -1,9 +1,17 @@
 package me.iron_fist222.custombosses.Bosses.Moves;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.level.Level;
+import org.bukkit.block.data.type.Bed;
 
-public class ParticleMoveTemplate extends Projectile {
+public class ParticleMoveTemplate extends Entity {
 
+    ParticleMoveTemplate(Level world){
+        super(EntityType<ParticleMoveTemplate>)
+
+    }
 
 
 
